1297f6aba7f6548d475ec7b94fb8e3ab3b986092
kb@triarc-labs.com <kb@triarc-labs.com>
2015-12-01 11:56:29 +0100

    Bumped version to 0.2.0

--- tlTreeView.js
+++ tlTreeView.js
@@ -4,50 +4,57 @@ var Triarc;
     (function (Web) {
         var mod = angular.module("tlTreeView", []);
         mod.factory("cirrusTreeViewService", function () {
-            var factory = {};
-            factory.selectedNode = null;
-            factory.unselectNode = function () {
-                if (factory.selectedNode)
-                    factory.selectedNode.selected = undefined;
-                factory.selectedNode = null;
-            };
-            factory.selectNode = function (node) {
-                if (factory.selectedNode)
-                    factory.selectedNode.selected = undefined;
-                factory.selectedNode = node;
-                node.selected = true;
-            };
-            factory.toggleNode = function (node) {
-                // no node selected
-                if (!node)
-                    return;
-                // no children
-                if (!node.children)
-                    return;
-                // collapse / expand
-                if (node.children && node.children.length > 0) {
-                    node.collapsed = !node.collapsed;
-                }
-            };
-            factory.toggleAll = function (node) {
-                // no node selected
-                if (!node)
-                    return;
-                // set all children equal to what the parent will be, 
-                // else can get out of sync
-                var collapsed = !node.collapsed;
-                var iterate = function (child) {
-                    if (!child.children) {
+            var factory = {
+                selectedNode: null,
+                unselectNode: function () {
+                    if (factory.selectedNode)
+                        factory.selectedNode.selected = undefined;
+                    factory.selectedNode = null;
+                },
+                selectNode: function (node) {
+                    if (factory.selectedNode)
+                        factory.selectedNode.selected = undefined;
+                    factory.selectedNode = node;
+                    node.selected = true;
+                },
+                toggleNode: function (node) {
+                    // no node selected
+                    if (!node)
                         return;
+                    // no children
+                    if (!node.children)
+                        return;
+                    // collapse / expand
+                    if (node.children && node.children.length > 0) {
+                        node.collapsed = !node.collapsed;
                     }
-                    else {
-                        child.collapsed = collapsed;
-                        for (var i = 0; i < child.children.length; i++) {
-                            iterate(child.children[i]);
+                },
+                openNode: function (node) {
+                    node.collapsed = false;
+                },
+                closeNode: function (node) {
+                    node.collapsed = true;
+                },
+                toggleAll: function (node) {
+                    // no node selected
+                    if (!node)
+                        return;
+                    // set all children equal to what the parent will be, 
+                    // else can get out of sync
+                    var collapsed = !node.collapsed;
+                    var iterate = function (child) {
+                        if (!child.children) {
+                            return;
                         }
-                    }
-                };
-                iterate(node);
+                        else {
+                            child.collapsed = collapsed;
+                            for (var i = 0; i < child.children.length; i++) {
+                                iterate(child.children[i]);
+                            }
+                        }
+                    };
+                    iterate(node);
+                }
             };
             return factory;
         });
