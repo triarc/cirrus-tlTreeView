e91286a325a101a76143e534c0d8a2c1aa062c71
kb@triarc-labs.com <kb@triarc-labs.com>
2015-12-01 13:50:08 +0100

    Bumped version to 0.2.1

--- tlTreeView.d.ts
+++ tlTreeView.d.ts
@@ -1,2 +1,11 @@
 declare module Triarc.Web {
+    interface ITreeViewServiceFactory {
+        selectedNode: any;
+        unselectNode(): void;
+        toggleNode(node: any): void;
+        toggleAll(node: any): void;
+        selectNode(node: any): void;
+        openNode(node: any): void;
+        closeNode(node: any): void;
+    }
 }
